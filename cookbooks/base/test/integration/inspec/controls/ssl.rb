50834a3968b154c6534935f74bb62ef2e4a5d7fe
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-05-16 11:52:53 -0400

    Make extra SSL checking conditional on not-Vagrant
    The ssl module doesn't handle target hosts.

--- cookbooks/base/test/integration/inspec/controls/ssl.rb
+++ cookbooks/base/test/integration/inspec/controls/ssl.rb
@@ -1,3 +1,13 @@
 # Test SSL
 
-include_controls 'ssl-baseline'
+is_vagrant = attribute(
+  'is_vagrant',
+  description: 'If running under Vagrant',
+  default: false
+)
+
+if is_vagrant != true
+  # Doesn't work in Vagrant. :(
+  include_controls 'ssl-baseline'
+end
+
