9542500aa32d7513d8e875be0ac6d3050e2ede55
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-05-16 16:13:53 -0400

    test for weak ciphers

--- cookbooks/site-nginx/test/integration/inspec/controls/ssl.rb
+++ cookbooks/site-nginx/test/integration/inspec/controls/ssl.rb
@@ -1,3 +1,6 @@
+
+cipher_list = command('openssl ciphers').stdout.split(':')
+
 control 'ssl-1' do
   impact 0.7
   title 'Disallowed SSL/TLS protocols'
@@ -13,6 +16,54 @@ control 'ssl-1' do
   end
   describe command('openssl s_client -ssl3 -connect 127.0.0.1:443 < /dev/null') do
     its('exit_status') { should eq 1 }
+  end
+
+end
+
+control 'ssl-2' do
+  impact 0.7
+  title 'Disallowed SSL/TLS ciphers'
+  desc '
+    Test for deprecated SSL/TLS protocols being enabled.
+  '
+
+  tag 'network'
+  ref 'Compliance: Network Accessibility', url: 'https://wiki.example.com/security/compliance/ops/Network+Accessibility'
+
+  cipher_list.select { |x| x =~ /DES/ }.each do |c|
+    describe command("openssl s_client -cipher #{c} -connect 127.0.0.1:443 < /dev/null") do
+      its('exit_status') { should eq 1 }
+    end
+  end
+
+  cipher_list.select { |x| x =~ /MD5/ }.each do |c|
+    describe command("openssl s_client -cipher #{c} -connect 127.0.0.1:443 < /dev/null") do
+      its('exit_status') { should eq 1 }
+    end
+  end
+
+  cipher_list.select { |x| x =~ /PSK/ }.each do |c|
+    describe command("openssl s_client -cipher #{c} -connect 127.0.0.1:443 < /dev/null") do
+      its('exit_status') { should eq 1 }
+    end
+  end
+
+  cipher_list.select { |x| x =~ /RC4/ }.each do |c|
+    describe command("openssl s_client -cipher #{c} -connect 127.0.0.1:443 < /dev/null") do
+      its('exit_status') { should eq 1 }
+    end
+  end
+
+  cipher_list.select { |x| x =~ /EXPORT/ }.each do |c|
+    describe command("openssl s_client -cipher #{c} -connect 127.0.0.1:443 < /dev/null") do
+      its('exit_status') { should eq 1 }
+    end
+  end
+
+  cipher_list.select { |x| x =~ /NULL/ }.each do |c|
+    describe command("openssl s_client -cipher #{c} -connect 127.0.0.1:443 < /dev/null") do
+      its('exit_status') { should eq 1 }
+    end
   end
 
 end
