2a7d0181a81bac63ca355340e2ae890939189ff6
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-05-15 21:16:43 -0400

    Add some network access compliance tests.

---
+++ cookbooks/base/test/integration/inspec/compliance/controls/network.rb
@@ -0,0 +1,38 @@
+# Check open ports
+#
+
+default_allowed_ports = attribute(
+  'base_compliance_allowed_ports',
+  description: 'List of allowed ports',
+  default: []
+)
+
+service_allowed_ports = attribute(
+  'service_compliance_allowed_ports',
+  description: 'List of allowed ports for a service.',
+  default: []
+)
+
+allowed_ports = default_allowed_ports + service_allowed_ports
+
+control 'network-1' do
+  impact 0.8
+  title 'Ensure only allowed ports are network accessible'
+  desc '
+    Only allowed ports should be network accessible.
+  '
+
+  tag 'network'
+  ref 'Compliance: Network Accessibility', url: 'https://wiki.example.com/security/compliance/ops/Network+Accessibility'
+
+  ports = command('netstat -ant -A inet | tail -n +3 | awk \'{print $4}\' | grep -v 127.0.0.1 | cut -d : -f 2').stdout.strip.lines.sort!.uniq!
+  puts ports
+  int_ports = ports.map { |i| i.to_i }
+  (int_ports - allowed_ports).each do |p|
+    describe port(p) do
+      it { should_not be_listening }
+    end
+  end
+end
+
+
